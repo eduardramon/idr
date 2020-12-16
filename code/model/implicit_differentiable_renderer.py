7e07859dd506a65cf447c575c78e80b093959b5b
Eduard Ramon Maldonado <eduard.ramon.maldonado@gmail.com>
2020-12-16 12:51:54 +0100

    Factoring out global feature vector into another network

--- code/model/implicit_differentiable_renderer.py
+++ code/model/implicit_differentiable_renderer.py
@@ -8,10 +8,9 @@ from model.embedder import *
 from model.ray_tracing import RayTracing
 from model.sample_network import SampleNetwork
 
-class ImplicitNetwork(nn.Module):
+class GeometryNetwork(nn.Module):
     def __init__(
             self,
-            feature_vector_size,
             d_in,
             d_out,
             dims,
@@ -23,7 +22,7 @@ class ImplicitNetwork(nn.Module):
     ):
         super().__init__()
 
-        dims = [d_in] + dims + [d_out + feature_vector_size]
+        dims = [d_in] + dims + [d_out]
 
         self.embed_fn = None
         if multires > 0:
@@ -97,6 +96,54 @@ class ImplicitNetwork(nn.Module):
             only_inputs=True)[0]
         return gradients.unsqueeze(1)
 
+class MaterialsNetwork(nn.Module):
+    def __init__(
+            self,
+            d_in,
+            d_out,
+            dims,
+            weight_norm=True,
+            multires=0
+    ):
+        super().__init__()
+
+        dims = [d_in] + dims + [d_out]
+
+        self.embed_fn = None
+        if multires > 0:
+            embed_fn, input_ch = get_embedder(multires)
+            self.embed_fn = embed_fn
+            dims[0] = input_ch
+
+        self.num_layers = len(dims)
+
+        for l in range(0, self.num_layers - 1):
+
+            lin = nn.Linear(dims[l], dims[l + 1])
+
+            if weight_norm:
+                lin = nn.utils.weight_norm(lin)
+
+            setattr(self, "lin" + str(l), lin)
+
+        self.softplus = nn.Softplus(beta=100)
+
+    def forward(self, input):
+        if self.embed_fn is not None:
+            input = self.embed_fn(input)
+
+        x = input
+
+        for l in range(0, self.num_layers - 1):
+            lin = getattr(self, "lin" + str(l))
+            x = lin(x)
+
+            if l < self.num_layers - 2:
+                x = self.softplus(x)
+
+        return x
+
+
 class RenderingNetwork(nn.Module):
     def __init__(
             self,
@@ -160,9 +207,9 @@ class RenderingNetwork(nn.Module):
 class IDRNetwork(nn.Module):
     def __init__(self, conf):
         super().__init__()
-        self.feature_vector_size = conf.get_int('feature_vector_size')
-        self.implicit_network = ImplicitNetwork(self.feature_vector_size, **conf.get_config('implicit_network'))
-        self.rendering_network = RenderingNetwork(self.feature_vector_size, **conf.get_config('rendering_network'))
+        self.geometry_network = GeometryNetwork(**conf.get_config('geometry_network'))
+        self.materials_network = MaterialsNetwork(**conf.get_config('materials_network'))
+        self.rendering_network = RenderingNetwork(**conf.get_config('rendering_network'))
         self.ray_tracer = RayTracing(**conf.get_config('ray_tracer'))
         self.sample_network = SampleNetwork()
         self.object_bounding_sphere = conf.get_float('ray_tracer.object_bounding_sphere')
@@ -179,17 +226,17 @@ class IDRNetwork(nn.Module):
 
         batch_size, num_pixels, _ = ray_dirs.shape
 
-        self.implicit_network.eval()
+        self.geometry_network.eval()
         with torch.no_grad():
-            points, network_object_mask, dists = self.ray_tracer(sdf=lambda x: self.implicit_network(x)[:, 0],
+            points, network_object_mask, dists = self.ray_tracer(sdf=lambda x: self.geometry_network(x)[:, 0],
                                                                  cam_loc=cam_loc,
                                                                  object_mask=object_mask,
                                                                  ray_directions=ray_dirs)
-        self.implicit_network.train()
+        self.geometry_network.train()
 
         points = (cam_loc.unsqueeze(1) + dists.reshape(batch_size, num_pixels, 1) * ray_dirs).reshape(-1, 3)
 
-        sdf_output = self.implicit_network(points)[:, 0:1]
+        sdf_output = self.geometry_network(points)[:, 0:1]
         ray_dirs = ray_dirs.reshape(-1, 3)
 
         if self.training:
@@ -211,10 +258,10 @@ class IDRNetwork(nn.Module):
 
             points_all = torch.cat([surface_points, eikonal_points], dim=0)
 
-            output = self.implicit_network(surface_points)
+            output = self.geometry_network(surface_points)
             surface_sdf_values = output[:N, 0:1].detach()
 
-            g = self.implicit_network.gradient(points_all)
+            g = self.geometry_network.gradient(points_all)
             surface_points_grad = g[:N, 0, :].clone().detach()
             grad_theta = g[N:, 0, :]
 
@@ -248,11 +295,11 @@ class IDRNetwork(nn.Module):
         return output
 
     def get_rbg_value(self, points, view_dirs):
-        output = self.implicit_network(points)
-        g = self.implicit_network.gradient(points)
+
+        feature_vectors = self.materials_network(points)
+        g = self.geometry_network.gradient(points)
         normals = g[:, 0, :]
 
-        feature_vectors = output[:, 1:]
         rgb_vals = self.rendering_network(points, normals, view_dirs, feature_vectors)
 
         return rgb_vals
